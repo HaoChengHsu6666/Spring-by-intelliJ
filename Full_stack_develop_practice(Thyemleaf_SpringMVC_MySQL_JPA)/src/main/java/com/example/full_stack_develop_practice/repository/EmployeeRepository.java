666525bbff5fa7c80de82666d5a744f25946c75d
HaoChengHsu6666 <87864835+HaoChengHsu6666@users.noreply.github.com>
2022-02-16 15:44:44 +0800

    Add files via upload

---
+++ Full_stack_develop_practice(Thyemleaf_SpringMVC_MySQL_JPA)/src/main/java/com/example/full_stack_develop_practice/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.example.full_stack_develop_practice.repository;
+
+import com.example.full_stack_develop_practice.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}
---
+++ Full_stack_develop_practice(Thyemleaf_SpringMVC_MySQL_JPA)/src/test/java/com/example/full_stack_develop_practice/FullStackDevelopPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.full_stack_develop_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackDevelopPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
